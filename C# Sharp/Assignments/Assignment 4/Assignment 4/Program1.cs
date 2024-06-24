8a3c32ad790ce9645e6b2bf3dce9ed2ffc0707b1
Santhosh Kenchanagoudar <santhoshke@infinite.com>
2024-06-24 23:08:26 +0530

    Assignment 4 solns

---
+++ C# Sharp/Assignments/Assignment 4/Assignment 4/Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Person
+    {
+    private string firstName;
+    private string lastName;
+
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public static void Display(Person person)
+    {
+        string upperFirstName = person.firstName.ToUpper();
+        string upperLastName = person.lastName.ToUpper();
+
+        Console.WriteLine(upperFirstName);
+        Console.WriteLine(upperLastName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Person person = new Person("Santhosh", "Kenchanagoudar");
+
+        Person.Display(person);
+
+        Console.ReadLine();
+    }
+}
+
+}
---
+++ C# Sharp/Assignments/Assignment 4/Assignment 4/Program2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program2
+    {
+        static int CountLetterOccurrences(string str, char letter)
+        {
+            int count = 0;
+            foreach (char ch in str)
+            {
+                if (ch == letter)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string userString = Console.ReadLine();
+
+            Console.Write("Enter the letter to count: ");
+            char letterToCount;
+            if (!char.TryParse(Console.ReadLine(), out letterToCount))
+            {
+                Console.WriteLine("Please enter a single character for the letter to count.");
+                return;
+            }
+
+            int occurrences = CountLetterOccurrences(userString, letterToCount);
+
+            Console.WriteLine($"The letter '{letterToCount}' appears {occurrences} times in the string.");
+
+            Console.ReadLine();
+        }
+    }
+}
