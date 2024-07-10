c2d67e79472bc779203a5e57c0093abbc8c276a8
Santhosh Kenchanagoudar <santhoshke@infinite.com>
2024-07-10 11:05:21 +0530

    cc4 Assessment solns

---
+++ C# Sharp/Assessments/cc4/cc4/CalculatorApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc4
+{
+    // Define a delegate that takes two integers and returns an integer
+    delegate int CalDelegate(int x, int y);
+
+    class CalculatorApp
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first integer: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second integer: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------");
+            CalDelegate add = new CalDelegate(Add);
+            CalDelegate subtract = new CalDelegate(Sub);
+            CalDelegate multiply = new CalDelegate(Mul);
+
+            Console.WriteLine($"Addition: {num1} + {num2} = {add(num1, num2)}");
+            Console.WriteLine($"Subtraction: {num1} - {num2} = {subtract(num1, num2)}");
+            Console.WriteLine($"Multiplication: {num1} * {num2} = {multiply(num1, num2)}");
+
+            Console.Read();
+        }
+
+        // Method for addition
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        // Method for subtraction
+        static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        // Method for multiplication
+        static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ C# Sharp/Assessments/cc4/cc4/TextToAppend.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc4
+{
+    class TextToAppend
+    {
+        static void Main()
+        {
+            string filePath = "C:\\Users\\santhoshke\\Infinite_June24\\C# Sharp\\Assessments\\cc4\\MyExample.txt";
+            Console.Write("Enter the text to append: ");
+            string textToAppend = Console.ReadLine();
+
+
+            AppendTextToFile(filePath, textToAppend);
+            Console.WriteLine("Text appended to the file.");
+
+            Console.Read();
+        }
+
+        static void AppendTextToFile(string filePath, string textToAppend)
+        {
+            try
+            {
+                // Append text to the file, if the file doesn't exist, it will create a new one
+                using (StreamWriter sw = new StreamWriter(filePath, true))
+                {
+                    sw.WriteLine(textToAppend);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
