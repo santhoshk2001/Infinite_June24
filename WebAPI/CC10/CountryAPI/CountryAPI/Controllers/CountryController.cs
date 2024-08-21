a2e5d86a6fdfec3c1b8217401a1c76e311fe44a2
santhoshk2001 <santhoshke@infinite.com>
2024-08-21 10:54:18 +0530

    CC10 assessment

---
+++ WebAPI/CC10/CountryAPI/CountryAPI/Controllers/CountryController.cs
@@ -0,0 +1,71 @@
+﻿using CountryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace CountryAPI.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static List<Country> countries = new List<Country>
+             {
+                 new Country { ID = 1, CountryName = "INDIA", Capital = "Delhi" },
+                 new Country { ID = 2, CountryName = "US", Capital = "Washington" }
+             };
+
+        // GET: api/country
+        public IEnumerable<Country> GetCountries()
+        {
+            return countries;
+        }
+
+        // GET: api/country/1
+        public IHttpActionResult GetCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        // POST: api/country
+        public IHttpActionResult PostCountry(Country country)
+        {
+            country.ID = countries.Max(c => c.ID) + 1;
+            countries.Add(country);
+            return CreatedAtRoute("DefaultApi", new { id = country.ID }, country);
+        }
+
+        // PUT: api/country/1
+        public IHttpActionResult PutCountry(int id, Country updatedCountry)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            country.CountryName = updatedCountry.CountryName;
+            country.Capital = updatedCountry.Capital;
+            return StatusCode(System.Net.HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/country/1
+        public IHttpActionResult DeleteCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            countries.Remove(country);
+            return StatusCode(System.Net.HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ WebAPI/CC10/CountryAPI/CountryAPI/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace CountryAPI.Models
+{
+    public class Country
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CountryName { get; set; }
+
+        [StringLength(100)]
+        public string Capital { get; set; }
+    }
+}
